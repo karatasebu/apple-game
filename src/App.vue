75f3d5b8df70184b7370afeddf10da6555aa4e60
Ebubekir Karataş <karatasebu47@gmail.com>
2021-12-30 10:37:56 +0300

    body::after removed

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,6 @@ body {
   font-family: sans-serif;
 }
 body {
-  position: relative;
-}
-body::after {
-  content: "";
-  position: absolute;
-  inset: 0;
   background-image: url("https://images.unsplash.com/photo-1518717202715-9fa9d099f58a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=80");
   z-index: -1;
   background-size: 100% 100%;
